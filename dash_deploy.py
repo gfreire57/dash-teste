41037d25cddb91e44b4157bfaf07c5e64f096011
Gabriel Hasmann <gfreire57@gmail.com>
2024-06-02 17:34:07 -0300

    Add files via upload

---
+++ dash_deploy.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("Visualização dos dados")
+
+arquivo = st.file_uploader("Escolha um arquivo CSV", type='csv')
+
+if arquivo:
+    df = pd.read_csv(arquivo)
+    st.write(df)
+
+    st.subheader('Selecione as colunas para o gráfico')
+    opcoes = df.columns.tolist()
+    opcoes_wo_data = [x for x in opcoes if x != 'Data']
+    eixo_x = st.selectbox("eixo x", options=opcoes)
+    eixo_y = st.selectbox("eixo y", options=opcoes_wo_data)
+
+    if eixo_x and eixo_y:
+        fig = px.line(df, x=eixo_x, y=eixo_y, title='Dados')
+        st.plotly_chart(fig)
+
+        st.subheader("gráfico de barra")
+        fig2 = px.bar(df, x='Data', y='Vendas', title='Barra')
+        st.plotly_chart(fig2)
+
